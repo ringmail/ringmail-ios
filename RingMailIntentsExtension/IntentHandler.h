b4fe21e7fae83f7fe6035988695998ae7dfa1092
Mark Baxter <mark@Marks-iMac.local>
2016-10-06 16:57:42 -0700

    add intents ext

---
+++ RingMailIntentsExtension/IntentHandler.h
@@ -0,0 +1,13 @@
+//
+//  IntentHandler.h
+//  RingMailIntentsExtension
+//
+//  Created by Mark Baxter on 10/6/16.
+//
+//
+
+#import <Intents/Intents.h>
+
+@interface IntentHandler : INExtension
+
+@end
