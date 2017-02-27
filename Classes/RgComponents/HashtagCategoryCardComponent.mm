4566a323e5905a4250a11412f2c441b1d402ddce
Mark Baxter <mbaxter@dyl.com>
2017-02-27 14:10:20 -0800

    mod border charcol

--- Classes/RgComponents/HashtagCategoryCardComponent.mm
+++ Classes/RgComponents/HashtagCategoryCardComponent.mm
@@ -45,7 +45,7 @@
                     {CKComponentViewAttribute::LayerAttribute(@selector(setCornerRadius:)), @20.0},
                     {@selector(setClipsToBounds:), @YES},
                     {@selector(setBackgroundColor:), [UIColor whiteColor]},
-                    {CKComponentViewAttribute::LayerAttribute(@selector(setBorderColor:)), (id)[[UIColor colorWithHex:@"#000000"] CGColor]},
+                    {CKComponentViewAttribute::LayerAttribute(@selector(setBorderColor:)), (id)[[UIColor colorWithHex:@"#222222"] CGColor]},
                     {CKComponentViewAttribute::LayerAttribute(@selector(setBorderWidth:)), 1 / [UIScreen mainScreen].scale},
                 }}
             size:{.width = rectWidth}
@@ -86,7 +86,7 @@ CKInsetComponent* hashtagCatInsetLabelComponent(NSDictionary* data)
     return
     [
          CKInsetComponent newWithInsets:
-            {.left = 10, .right = 10, .top = 8, .bottom = INFINITY}
+            {.left = 10, .right = 10, .top = 6, .bottom = INFINITY}
             component: [
                 CKLabelComponent newWithLabelAttributes:{
                     .string = [data objectForKey:@"name"],
