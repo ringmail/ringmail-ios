ce41b601483bb8fad715c518dd5638140d45737c
Mark Baxter <mbaxter@dyl.com>
2017-05-25 16:05:32 -0700

    mod contacts detail table

--- Classes/LinphoneUI/UIContactDetailsOptions.m
+++ Classes/LinphoneUI/UIContactDetailsOptions.m
@@ -18,11 +18,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView *lineView = [[UIView alloc] initWithFrame:CGRectMake(15, self.view.frame.size.height - 1, self.view.frame.size.width - 30, 1)];
-    lineView.backgroundColor = [UIColor lightGrayColor];
-    [inviteButton addSubview:lineView];
-    [shareContactButton addSubview:lineView];
-    [shareLocationButton addSubview:lineView];
+    UIView *lineView1 = [[UIView alloc] initWithFrame:CGRectMake(0, inviteButton.frame.size.height - 2, self.view.frame.size.width - 30, 1)];
+    lineView1.backgroundColor = [UIColor lightGrayColor];
+    UIView *lineView2 = [[UIView alloc] initWithFrame:CGRectMake(0, shareContactButton.frame.size.height - 2, self.view.frame.size.width - 30, 1)];
+    lineView1.backgroundColor = [UIColor lightGrayColor];
+    UIView *lineView3 = [[UIView alloc] initWithFrame:CGRectMake(0, shareLocationButton.frame.size.height - 2, self.view.frame.size.width - 30, 1)];
+    lineView1.backgroundColor = [UIColor lightGrayColor];
+    lineView2.backgroundColor = [UIColor lightGrayColor];
+    lineView3.backgroundColor = [UIColor lightGrayColor];
+    [inviteButton addSubview:lineView1];
+    [shareContactButton addSubview:lineView2];
+    [shareLocationButton addSubview:lineView3];
 }
 
 - (void)didReceiveMemoryWarning {
